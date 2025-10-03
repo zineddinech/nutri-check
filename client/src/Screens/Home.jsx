ca54b00677e0af5d726fd195c8216c8f2438ad70
toureydaou <ttoureydaou@gmail.com>
2025-10-03 11:07:06 +0200

    Background et début du projet

---
+++ client/src/Screens/Home.jsx
@@ -0,0 +1,13 @@
+import "./../Screens_CSS/Home.css";
+import "./../Screens_CSS/Background.css";
+
+function Home() {
+  return (
+    <div className="overlay">
+      <h1>Bienvenue sur notre boutique</h1>
+      <p>Faites vos courses en ligne facilement</p>
+    </div>
+  );
+}
+
+export default Home;

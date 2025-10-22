61dcab76b4b392448b00c353ebd6882df8726baa
VILA Rodrigo <r94vila@gmail.com>
2025-10-22 23:30:14 +0200

    feat(profil): implement /profil/getAllergiesByName endpoint

--- server/app/api/api.py
+++ server/app/api/api.py
@@ -2,7 +2,6 @@ from fastapi import APIRouter
 
 from .endpoints import product, user, profil
 
-
 api_router = APIRouter()
 
 # --- Enregistrement des routeurs ---
@@ -10,3 +9,4 @@ api_router.include_router(user.router, prefix="/users", tags=["users"])
 api_router.include_router(product.router, prefix="/product", tags=["product"])
 api_router.include_router(profil.router, prefix="/profil", tags=["profil"])
 api_router.include_router(product.router, prefix="/products", tags=["products"])
+api_router.include_router(profil.router, prefix="/profil", tags=["profil"])

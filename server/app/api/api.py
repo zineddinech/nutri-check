d13b880bca49ba14c14df923e4828c17b31ebfc7
toureydaou <ttoureydaou@gmail.com>
2025-10-24 10:03:05 +0200

    fix:(router and dependencies)

--- server/app/api/api.py
+++ server/app/api/api.py
@@ -5,9 +5,6 @@ from .endpoints import product, user, profil
 api_router = APIRouter()
 
 # --- Enregistrement des routeurs ---
-api_router.include_router(user.router, prefix="/profil", tags=["users"])
-api_router.include_router(
-    product.router, prefix="/products", tags=["products"])
 api_router.include_router(user.router, prefix="/users", tags=["users"])
 api_router.include_router(product.router, prefix="/product", tags=["product"])
 api_router.include_router(profil.router, prefix="/profil", tags=["profil"])

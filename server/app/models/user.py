1fcf83dab1bc0d8c4931071d7a038b54cc267d18
Hiba <116457271+hibaoqba@users.noreply.github.com>
2025-11-05 16:22:46 +0100

    added favorites

--- server/app/models/user.py
+++ server/app/models/user.py
@@ -25,6 +25,7 @@ class User(BaseModel):
     hashed_password: str
     is_active: bool = True
     allergies: List[str] = []
+    favorites: List[str] = [] 
     created_at: Optional[str] = None
     updated_at: Optional[str] = None
 

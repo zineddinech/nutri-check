df7f5da7be55a94a9d19bbd2ad6ab64896ec95ad
Hiba <116457271+hibaoqba@users.noreply.github.com>
2025-11-05 16:22:46 +0100

    added favorites

--- server/app/api/api.py
+++ server/app/api/api.py
@@ -1,10 +1,12 @@
 from fastapi import APIRouter
 
-from .endpoints import product, profil, user
+from .endpoints import product, profil, user, shopping_trip
 
 api_router = APIRouter()
 
 # --- Enregistrement des routeurs ---
+api_router.include_router(shopping_trip.router, prefix="/shoppingTrips", tags=["shoppingTrips"])
+
 api_router.include_router(user.router, prefix="/users", tags=["users"])
 api_router.include_router(product.router, prefix="/product", tags=["product"])
 api_router.include_router(profil.router, prefix="/profil", tags=["profil"])
